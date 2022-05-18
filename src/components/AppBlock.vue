b44b9e2caa9e85ce3bf67f05a932ed8e19ed6569
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-05-18 22:53:30 +0300

    created block component

---
+++ src/components/AppBlock.vue
@@ -0,0 +1,41 @@
+<template>
+    <section v-if="showBlock" class="shadow-2xl mt-6 bg-white rounded-3xl">
+        <div class="p-6 text-center sm:p-8">
+            <h5 class="mt-6 text-3xl font-bold">Quickly press the button below</h5>
+
+            <button @click="stopTimer" class="inline-block w-full py-4 mt-8 text-sm font-bold text-white bg-pink-600 rounded-full shadow-xl">Click</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0,
+        };
+    },
+    props: ['delay'],
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer);
+            this.$emit('time', this.reactionTime);
+        },
+    },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,40 @@
 <template>
-    <div class="home text-center">
+    <div class="flex justify-center items-center flex-col">
         <h1 class="title-gradient">Reaction Timer</h1>
+        <button @click="start" :class="{ 'disable-button': isPlaying }" class="start-button">Play</button>
+        <app-block v-if="isPlaying" :delay="delay" @time="endGame" />
+        <p class="result-text" v-if="showResult">ms : {{ score }}</p>
     </div>
 </template>
 
 <script>
 import AOS from 'aos';
+import AppBlock from '../components/AppBlock.vue';
 export default {
     name: 'HomeView',
+    components: {
+        AppBlock,
+    },
     mounted() {
         AOS.init();
     },
     data() {
         return {
-            showModal: false,
+            isPlaying: false,
+            delay: null,
+            score: null,
+            showResult: false,
         };
     },
     methods: {
-        toggleModal() {
-            this.showModal = !this.showModal;
+        start() {
+            this.delay = 2000 + Math.random() * 5000;
+            this.isPlaying = true;
+        },
+        endGame(reactionTime) {
+            this.score = reactionTime;
+            this.isPlaying = false;
+            this.showResult = true;
         },
     },
 };
